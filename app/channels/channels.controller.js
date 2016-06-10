aa6a3da9e04f11a8accd1b64ca0db15d5db89ec1
Louise <louise.beh@gmail.com>
2016-06-10 14:23:32 +0100

    Set channels and profile to the resolved dependencies from the router

--- app/channels/channels.controller.js
+++ app/channels/channels.controller.js
@@ -1,4 +1,7 @@
 angular.module('angularfireSlackApp')
   .controller('ChannelsCtrl', function($state, Auth, profile, channels){
     var channelsCtrl = this;
+
+    channelsCtrl.profile = profile;
+    channelsCtrl.channels = channels;
   });
