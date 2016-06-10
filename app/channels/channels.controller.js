9481584c18cb3246c330064a375830e620121e86
Louise <louise.beh@gmail.com>
2016-06-10 16:44:28 +0100

    Update createChannel function to send the user to the newly created channel upon creation

--- app/channels/channels.controller.js
+++ app/channels/channels.controller.js
@@ -11,13 +11,11 @@ angular.module('angularfireSlackApp')
     }
 
     channelsCtrl.createChannel = function(){
-      channelsCtrl.channels.$add(channelsCtrl.newChannel).then(function(){
-        channelsCtrl.newChannel = {
-          name: ''
-        };
+      channelsCtrl.channels.$add(channelsCtrl.newChannel).then(function(ref){
+        $state.go('channels.messages', {channelId: ref.key()});
       });
     };
-    
+
     channelsCtrl.logout = function(){
       Auth.$unauth();
       $state.go('home');
