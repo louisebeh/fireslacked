3b008ddb75bccb7297f4e2880a05388e5239661c
Louise <louise.beh@gmail.com>
2016-06-10 16:49:11 +0100

    Add a Firebase reference to the userMessages node

--- app/channels/messages.service.js
+++ app/channels/messages.service.js
@@ -1,6 +1,7 @@
 angular.module('angularfireSlackApp')
   .factory('Messages', function($firebaseArray, FirebaseUrl){
     var channelMessagesRef = new Firebase(FirebaseUrl+"channelMessages");
+    var userMessagesRef = new Firebase(FirebaseUrl+"userMessages");
 
     return {
       forChannel: function(channelId){
