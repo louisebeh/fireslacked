e88328a82eed28a1aee06d3c155d4d1e914a7362
Louise <louise.beh@gmail.com>
2016-06-10 16:52:26 +0100

    Add a forUsers function to retrieve direct messages between two users

--- app/channels/messages.service.js
+++ app/channels/messages.service.js
@@ -6,6 +6,11 @@ angular.module('angularfireSlackApp')
     return {
       forChannel: function(channelId){
         return $firebaseArray(channelMessagesRef.child(channelId));
+      },
+      forUsers: function(uid1, uid2){
+        var path = uid1 < uid2 ? uid1+'/'+uid2 : uid2+'/'+uid1;
+
+        return $firebaseArray(userMessagesRef.child(path));
       }
     };
   });
